801c7cf32de949eebc725ed40a77de8c39771e99
mpelzsherman <mpelzsherman@gmail.com>
2012-08-05 19:05:36 -0400

    Added support for actually copying files from the ActiveMedia repo into the destiation dir.

--- Exporter.php
+++ Exporter.php
@@ -8,16 +8,24 @@ class Exporter
             echo "Created $collectionDir\n";
         }
 
-        $this->extractFilesForCollection($DAO, $collection);
+        $this->extractFilesForCollection($DAO, $collection, $collectionDir);
 
         foreach($DAO->getChildCollections($collection) as $childCollection) {
             $this->processCollection($DAO, $childCollection, $collectionDir);
         }
     }
 
-    public function extractFilesForCollection($DAO, $collection) {
+    public function extractFilesForCollection($DAO, $collection, $collectionDir) {
         foreach ($DAO->getFilesForCollection($collection) as $file) {
-            echo $this->getFullPathForFile($file) . "\n";
+            $sourcePath = $this->getFullPathForFile($file);
+            $destDir = $collectionDir . DIRECTORY_SEPARATOR . $file['CATALOG_DATE'];
+            if (!is_dir($destDir)) {
+                mkdir($destDir);
+            }
+            $destPath = $destDir . DIRECTORY_SEPARATOR . $file['NAME'];
+            echo "copying $sourcePath to $destPath...";
+            copy($sourcePath, $destPath);
+            echo "\ndone!\n";
         }
 
     }
